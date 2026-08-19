c69680cc882f1f5d49bb2fb7d896596673834dc4
arihant-pixel-dragonian <ARIHANT.35296@stu.upes.ac.in>
2026-08-19 17:24:15 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,12 @@
+/*DATE: 06/08/2026
+ Sum of two numbers */
+ #include<stdio.h>
+ int main()
+ {
+ float a,b,Sum;
+ printf("Input two numbers in variables a and b\n");
+ scanf("%f %f", &a,&b);
+ Sum=a+b;
+ printf("Sum of a and b:%0.2f\n", Sum);
+ return 0;
+ }
